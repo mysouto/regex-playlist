2ea21613114ca2d61b0bb73d27c1ca61f61c9e34
Milena <65979426+mysouto@users.noreply.github.com>
2022-05-06 12:47:45 -0700

    create regular expressions for all input fields

--- validation.js
+++ validation.js
@@ -14,7 +14,14 @@ const inputs = document.querySelectorAll('input');
 
 // patterns is an object
 const patterns = {
-    telephone: /^\d{11}$/ 
+    // must match name property in html 
+    telephone: /^\d{11}$/,
+    username: /^[a-z\d]{5,12}$/i,
+    // password used \m metacharacter to include @- symbols
+    password: /^[\w@-]{8,20}$/,
+    slug: /^[a-z\d-]{8,20}$/,
+    // basic regex for an email has 4 parts: name, @, domain, dot., extension, dot.something (optional with?)
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 };
 
 // create validation function - 2 parameters (field, regex used to test against)
